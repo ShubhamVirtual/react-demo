6b71354779fabae33f509bcf218b871b372e2a32
ShubhamVirtual <shubhamghavari@virtualemployee.com>
2022-06-28 18:20:42 +0530

    login form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from "./Componants/Login/Login";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='login_page py-4'>
+        <div className='container py-4'>
+          <div className='row'>
+            <div className='col-lg-5'>
+                <Login />
+            </div>
+            <div className='col-lg-7 d-flex justify-content-end align-items-end'>
+              <img src="./assets/images/login.svg" className='img-fluid pull-right' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
